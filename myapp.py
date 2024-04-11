f0c4d594eee7bf2e0e26f6564958cb4d75b65720
Hélio do Nascimento <115810349+Puritoka@users.noreply.github.com>
2024-04-11 17:37:35 -0300

    Implementação do Bloco de comando responsavel por separar as colunas, agurpar itens repetidos e somar as quantidades

--- myapp.py
+++ myapp.py
@@ -4,17 +4,20 @@ import pandas as pd
 # Linha para ler uma tabela em Excel
 tabela_vendas = pd.read_excel('Vendas.xlsx')
 
-#  Visualização de Database
+# Visualização de Database
 # Exibir sem restrições todas as colunas da base de dados
 pd.set_option('display.max_columns', None)
 
 # Faturamento por Loja
-# Bloco de comando responsavel por Separar as colunas, agurpar itens repetidos e somar os valores'
+# Bloco de comando responsavel por Separar as colunas, agurpar itens repetidos e somar os valores
 faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
 print(faturamento)
 
-#  Quantidade de produtos vendidos por Loja
+# Quantidade de produtos vendidos por Loja
+# Bloco de comando responsavel por Separar as colunas, agurpar itens repetidos e somar os valores
+quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
 
-#  ‘Ticket’ médio de produtos vendidos por Loja
+# ‘Ticket’ médio de produtos vendidos por Loja
 
-#  Envio de Email com Relatório
+# Envio de Email com Relatório
