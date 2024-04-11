f8857f18a219a18ac4a696a7f0022bec6c7b0950
Hélio do Nascimento <115810349+Puritoka@users.noreply.github.com>
2024-04-11 13:21:54 -0300

    importação da base de dados em EXCEL e bibliotecas(pandas)

---
+++ myapp.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Importar base de Dados (database)
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+print(tabela_vendas)
+
+#  Visualização de Database
+
+#  Faturamento por Loja
+
+#  Quantidade de produtos vendidos por Loja
+
+#  ‘Ticket’ médio de produtos vendidos por Loja
+
+#  Envio de Email com Relatório
